c1ad6ab32563e8a5f320813d4ce2b9245be7c5c7
Geverson Souza <geversonjosedesouza@gmail.com>
2022-01-16 19:03:13 -0300

    Importação do css do PrimeReact

--- src/core/app/index.jsx
+++ src/core/app/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Routes from '../routes';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 const App = () => {
   return (
