38a7bf46acbd505e192b0da3fdec114b98ad5310
Geverson Souza <geversonjosedesouza@gmail.com>
2022-01-16 01:01:29 -0300

    Adicionando rotas de página inexistende e página inicial

--- src/core/routes/index.jsx
+++ src/core/routes/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Router, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../store';
 import { history } from '../history';
 import Unauthenticated from '../containers/unauthenticated';
+import Authenticated from '../containers/authenticated';
 import Login from '../../views/pages/usuario/Login';
 import CriarConta from '../../views/pages/usuario/CriarConta';
 import RecuperarSenha from '../../views/pages/usuario/RecuperarSenha';
+import PaginaInexistente from '../../views/pages/mystore/PaginaInexistente';
+import PaginaInicial from '../../views/pages/mystore/PaginaInicial';
 
 const Routes = () => {
     return (
@@ -14,9 +17,15 @@ const Routes = () => {
             <Router history={history}>
                 <BrowserRouter>
                     <Switch>
-                        <Route path="/" exact component={Unauthenticated(Login)} />
-                        <Route path="/criar-conta" exact component={Unauthenticated(CriarConta)} />
-                        <Route path="/recuperar-senha" exact component={Unauthenticated(RecuperarSenha)} />
+                         {/* Páginas sem autenticação */}
+                        <Route path="/mystore/" exact component={Unauthenticated(Login)} />
+                        <Route path="/mystore/criar-conta" exact component={Unauthenticated(CriarConta)} />
+                        <Route path="/mystore/recuperar-senha" exact component={Unauthenticated(RecuperarSenha)} />
+                        {/* Páginas com autenticação */}
+                        <Route path="/mystore/pagina-inicial" exact component={Authenticated(PaginaInicial)} />
+                        {/* Qualquer página que não esteja dentro das rotas especificadas */}
+                        <Route path="/mystore/pagina-inexistente" exact component={PaginaInexistente} />
+                        <Redirect from="*" to="/mystore/pagina-inexistente" />
                     </Switch>
                 </BrowserRouter>
             </Router>
