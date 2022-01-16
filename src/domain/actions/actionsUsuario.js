2662c91ba9343bcaafe494973bd1a31f2c8e840f
Geverson Souza <geversonjosedesouza@gmail.com>
2022-01-16 01:03:11 -0300

    Action e reducer do usuário

--- src/domain/actions/actionsUsuario.js
+++ src/domain/actions/actionsUsuario.js
@@ -1,19 +1,237 @@
 import axios from 'axios';
-import { USUARIO_ENTRAR } from '../../core/store/types';
-import { api } from '../../core/config';
-import { headers } from '../../core/store/localStorage';
+import {
+    BUSCAR_USUARIO,
+    LIMPAR_USUARIO,
+    LIMPAR_USUARIOS,
+    LISTAR_USUARIOS,
+    LOGIN_USUARIO,
+    LOGOUT_USUARIO,
+    PESQUISAR_USUARIOS
+} from '../../core/store/types';
+import { pass, url, user } from '../../core/config';
+import { salvarToken, buscarToken, headers, removerToken } from '../../core/store/localStorage';
 import errorHandler from '../../core/store/errorHandler';
+import { encode } from 'base-64';
 
-export const usuariosPesquisar = (usuarioFiltro, callback) => {
+export const alterarFotoPerfil = (dadosUsuario, callback) => {
     return (dispatch) => {
-        axios.get(`${api}/v1/usuarios/listar`, headers())
-        .then((response) => {
-            //Testes
-            console.log(JSON.stringify(usuarioFiltro));
-                dispatch({ type: USUARIO_ENTRAR, payload: response.data })
+        axios.put(`${url}/v1/usuarios/perfil-imagem`, dadosUsuario, headers())
+            .then((response) => {
+                callback({ erro: response.data });
             })
-            .catch((err) => {
-                callback(errorHandler(err));
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const alterarPerfil = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.put(`${url}/v1/usuarios/perfil`, dadosUsuario, headers())
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const alterarSenha = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.put(`${url}/v1/usuarios/nova-senha`, dadosUsuario)
+            .then((response) => {
+                removerToken();
+                dispatch({ type: LOGOUT_USUARIO });
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const alterarUsuario = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.put(`${url}/v1/usuarios/${dadosUsuario._id}`, dadosUsuario, headers())
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const criarConta = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.post(`${url}/v1/usuarios/criar-conta`, dadosUsuario)
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const ativarUsuario = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.put(`${url}/v1/usuarios/ativar/${dadosUsuario.id}`, { ativo: dadosUsuario.ativo }, headers())
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const getPerfil = (callback) => {
+    return (dispatch) => {
+        console.log("Salvando token getPerfil");
+        if (buscarToken()) {
+            axios.get(`${url}/v1/usuarios/perfil`, headers())
+                .then((response) => {
+                    //salvarToken(response.data);
+                    dispatch({ type: LOGIN_USUARIO, payload: response.data })
+                })
+                .catch((callbackError) => {
+                    removerToken();
+                    dispatch({ type: LOGOUT_USUARIO });
+                    callback(errorHandler(callbackError))
+                });
+        } else {
+            removerToken();
+            dispatch({ type: LOGOUT_USUARIO });
+        }
+    }
+}
+
+export const getUsuario = (id) => {
+    return (dispatch) => {
+        axios.get(`${url}/v1/usuarios/${id}`, headers())
+            .then((response) => {
+                dispatch({ type: BUSCAR_USUARIO, payload: response.data });
+            })
+            .catch(errorHandler);
+    }
+}
+
+export const getUsuarios = () => {
+    return (dispatch) => {
+        axios.get(`${url}/v1/usuarios`, headers())
+            .then((response) => {
+                dispatch({ type: LISTAR_USUARIOS, payload: response.data });
+            })
+            .catch(errorHandler);
+    }
+}
+
+export const handleLogin = ({ email, senha }, callback) => {
+    return (dispatch) => {
+        axios.create({
+            baseURL: url,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Basic ${encode(`${user}:${pass}`)}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        }).post(
+            '/oauth/token',
+            `username=${email}&password=${senha}&grant_type=password`,
+        )
+            .then((response) => {
+                salvarToken(response.data.access_token);
+                dispatch({ type: LOGIN_USUARIO, payload: { usuario: response.data } })
+            })
+            .catch((callbackError) => callback(errorHandler(callbackError)));
+    }
+}
+
+export const handleLogout = () => {
+    removerToken();
+    return { type: LOGOUT_USUARIO };
+}
+
+export const limparUsuario = () => {
+    return (dispatch) => {
+        dispatch({ type: LIMPAR_USUARIO });
+    }
+}
+
+export const limparUsuarios = () => {
+    return (dispatch) => {
+        dispatch({ type: LIMPAR_USUARIOS });
+    }
+}
+
+export const pesquisarUsuarios = (nome, email) => {
+    return (dispatch) => {
+        axios.get(`${url}/v1/usuarios/pesquisar?nome=${nome}&email=${email}`, headers())
+            .then((response) => {
+                dispatch({ type: PESQUISAR_USUARIOS, payload: response.data });
+            })
+            .catch(errorHandler);
+    }
+}
+
+export const recarregaPerfil = (callback) => {
+    return function (dispatch) {
+        axios.get(`${url}/v1/usuarios/perfil`, headers())
+            .then((response) => {
+                dispatch({ type: LOGIN_USUARIO, payload: response.data });
+                callback({ erro: response.data });
+            })
+            .catch((erro) => {
+                dispatch({ type: LOGOUT_USUARIO });
+                callback(errorHandler(erro));
+            })
+    }
+}
+
+export const recuperarSenha = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.put(`${url}/v1/usuarios/recuperar-senha`, dadosUsuario)
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const removerUsuario = (id, callback) => {
+    return (dispatch) => {
+        axios.delete(`${url}/v1/usuarios/${id}`, headers())
+            .then((response) => {
+                callback({ erro: response.data });
             });
     }
 }
+
+export const salvarUsuario = (dadosUsuario, callback) => {
+    return (dispatch) => {
+        axios.post(`${url}/v1/usuarios`, dadosUsuario, headers())
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const validacaoRecuperarSenha = (recuperarSenha, callback) => {
+    return (dispatch) => {
+        axios.get(`${url}/v1/usuarios/recuperar-senha/${recuperarSenha}`,)
+            .then((response) => {
+                callback({ erro: response.data });
+            })
+            .catch((err) => callback(errorHandler(err)));
+    }
+}
+
+export const testeAction = () => {
+    return (dispatch) => {
+        console.log('Testando a action usuário');
+    }
+}
+// export const usuariosPesquisar = (usuarioFiltro, callback) => {
+//     return (dispatch) => {
+//         axios.get(`${url}/v1/v1/usuarios/listar`, headers())
+//         .then((response) => {
+//             //Testes
+//             console.log(JSON.stringify(usuarioFiltro));
+//                 dispatch({ type: USUARIO_ENTRAR, payload: response.data })
+//             })
+//             .catch((err) => {
+//                 callback(errorHandler(err));
+//             });
+//     }
+// }
--- src/domain/reducers/reducersUsuario.js
+++ src/domain/reducers/reducersUsuario.js
@@ -1,11 +1,44 @@
-import { USUARIO_PESQUISAR } from '../../core/store/types';
+import { BUSCAR_USUARIO, LIMPAR_USUARIO, LIMPAR_USUARIOS, LISTAR_USUARIOS, LOGIN_USUARIO, LOGOUT_USUARIO, PESQUISAR_USUARIOS } from '../../core/store/types';
 
 const reducersUsuario = (state = {}, action) => {
     switch (action.type) {
-        case USUARIO_PESQUISAR:
+        case BUSCAR_USUARIO:
             return {
                 ...state,
-                usuariosPesquisa: action.payload,
+                usuario: action.payload.usuario,
+            }
+        case LIMPAR_USUARIO:
+            return {
+                ...state,
+                usuario: null,
+            }
+        case LIMPAR_USUARIOS:
+            return {
+                ...state,
+                usuarios: null,
+                usuariosPesquisados: null
+            }
+        case LISTAR_USUARIOS:
+            return {
+                ...state,
+                usuarios: action.payload.usuarios,
+            }
+        case LOGIN_USUARIO:
+            return {
+                ...state,
+                usuarioLogado: action.payload.usuario,
+                authorized: true
+            }
+        case LOGOUT_USUARIO:
+            return {
+                ...state,
+                usuarioLogado: null,
+                authorized: false
+            }
+        case PESQUISAR_USUARIOS:
+            return {
+                ...state,
+                usuariosPesquisados: action.payload.usuarios,
             }
         default:
             return state;
