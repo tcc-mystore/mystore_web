4cd4deb27966bc028d2453c358458a2043b0d1b7
Geverson Souza <geversonjosedesouza@gmail.com>
2022-01-16 19:04:20 -0300

    Componente da Barra de ferramentas

---
+++ src/views/components/BarraDeFerramentas/index.jsx
@@ -0,0 +1,49 @@
+
+import React, { useRef } from 'react';
+import { Menubar } from 'primereact/menubar';
+import logo from '../../../assets/images/logo.png'
+import { Button } from 'primereact/button';
+import { TieredMenu } from 'primereact/tieredmenu';
+import { Avatar } from 'primereact/avatar';
+import { Link, Redirect } from 'react-router-dom';
+
+const BarraDeFerramentas = ({ handleLogout, dadosUsuario }) => {
+    const menu = useRef(null);
+    const items = [
+        {
+            label: 'Meus Dados',
+            icon: 'pi pi-fw pi-book',
+            command: () => perfil()
+        },
+        {
+            label: 'Sair',
+            icon: 'pi pi-fw pi-power-off',
+            command: () => handleLogout()
+        }
+    ];
+
+    const perfil = () => {
+        return <Link to='/mystore/usuario/perfil' />
+    }
+
+    return (
+        <>
+            {console.log('dadosUsuario')}
+            {console.log(dadosUsuario)}
+            <div>
+                <div className="card">
+                    <TieredMenu model={items} popup ref={menu} id="overlay_tmenu" />
+                    <Menubar
+                        start={
+                            <Avatar className="p-overlay-badge" image={logo} size="xlarge" onClick={() => <Redirect to="/mystore/pagina-inicial" />} />
+                        }
+                        end={<Button label={dadosUsuario ? dadosUsuario.nome_completo : "Usuário"} icon="pi pi-bars" onClick={(event) => menu.current.toggle(event)}
+                            aria-haspopup aria-controls="overlay_tmenu" />}
+                    />
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default BarraDeFerramentas;
