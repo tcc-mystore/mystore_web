fffccd812b1d7111462e0018f75d558013a60608
Geverson Souza <geversonjosedesouza@gmail.com>
2022-01-18 10:48:53 -0300

    Trocando componente para o PrimeReact

--- src/views/pages/mystore/PaginaInexistente/index.jsx
+++ src/views/pages/mystore/PaginaInexistente/index.jsx
@@ -1,8 +1,7 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
+import { Button } from 'primereact/button';
 import { Link } from 'react-router-dom';
 import './index.css';
-import HomeIcon from '@material-ui/icons/Home';
 import EmailIcon from '@material-ui/icons/Email';
 
 const PaginaInexistente = () => {
@@ -20,13 +19,10 @@ const PaginaInexistente = () => {
                 </div>
                 <div className="error-actions">
                     <span className='btn'>
-                        <Button variant="contained" startIcon={<HomeIcon />}>
-                            <Link to="/mystore/" variant="contained">
-                                Página Inicial
-                            </Link>
-                        </Button>
+                        <Link to="/mystore/" variant="contained" style={{ textDecoration: 'none' }}>
+                            <Button label="Página Inicial" icon="pi pi-home" />
+                        </Link>
                     </span>
-
                     <span className='btn'>
                         <Button className='btn' onClick={() => (alert("Tem que desenvolver um componente pra cá!"))} variant="contained" startIcon={<EmailIcon />}>
                             Contate o Suporte
