2d77953b1231c635e3e943d62fdb15d99a2557a3
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-01-24 20:17:03 -0300

    Configuracões da API.

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
 import { encode } from 'base-64';
 import { pass, pass_manager, url, user, user_manager } from '.././config';
+import errorHandler from '../../core/store/errorHandler';
 
 const api = (token) => {
-    try {
-        return axios.create({
-            baseURL: url,
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            }
-        });
-    } catch (error) {
-        console.log(`Erro ao conectar na API -> ${new Date()} -> erro: ${error}`);
-    }
+    return axios.create({
+        baseURL: url,
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    });
 }
 
-const authorizationServerLogin = async (email, senha) => {
-    try {
-        return await axios.create({
+const authorizationServerLogin = ({ email, senha }, callback) => {
+    return (dispatch) => {
+        axios.create({
             baseURL: url,
             headers: {
                 'Accept': 'application/json',
@@ -29,59 +26,21 @@ const authorizationServerLogin = async (email, senha) => {
             '/oauth/token',
             `username=${email}&password=${senha}&grant_type=password`,
         ).then((response) => {
-            console.log(`Token de acesso gerado com sucesso!`);
-            return response.data;
-        }).catch((error) => {
-            console.log(`Não foi possível gerar o token de acesso!`);
-            console.log(JSON.stringify(url))
-            if (error.response) {
-                return {
-                    codigo: error.response.status,
-                    erro: error.response.data.error,
-                    mensagem: error.response.data.error_description,
-                }
-            }else{
-                return {
-                    codigo: 503,
-                    erro: error.name,
-                    mensagem: error.message,
-                }
-            }
-        });
-    } catch (error) {
-        console.log(`Erro ao gerar o token de acesso -> ${new Date()} -> erro: ${error}`);
+            callback({ payload: response.data });
+        }).catch(
+            (error) => callback(errorHandler(error))
+        );
     }
 }
 
-const authorizationServerRecuperarSenha = async () => {
-    try {
-        return await axios.create({
-            baseURL: url,
-            headers: { 
-                'Authorization': `Basic ${encode(`${user_manager}:${pass_manager}`)}`, 
-                'Content-Type': 'application/x-www-form-urlencoded' 
-            },
-        }).post(
-            '/oauth/token',
-            `grant_type=client_credentials`
-        ).then((response) => {
-            console.log(`Token de recuperação de senha gerado com sucesso!${response.data}`);
-            return response.data;
-        }).catch((error) => {
-            console.log(`Erro ao gerar token de recuperação de senha!`);
-            if (error.response) {
-                return {
-                    codigo: error.response.status,
-                    erro: error.response.data.error,
-                    mensagem: error.response.data.error_description,
-                }
-            } else {
-                throw error;
-            }
-        });
-    } catch (error) {
-        console.log(`Erro ao gerar token de recuperação de senha -> ${new Date()} -> erro: ${error.response}`);
-    }
+const authorizationServerRecuperarSenha = () => {
+    return axios.create({
+        baseURL: url,
+        headers: {
+            'Authorization': `Basic ${encode(`${user_manager}:${pass_manager}`)}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+    });
 }
 
 export { authorizationServerLogin, authorizationServerRecuperarSenha, api };
