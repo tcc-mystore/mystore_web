37fbecf7f5255c5a7e0633e093197a805f9ff578
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-02-14 06:26:29 -0300

    Refatorando arquivos criados pelo react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './core/app';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
