66eaac060cb0dd9934e955b3dcdd3cc3ff68a2e3
˜GeversonSouza <geversonjosedesouza@gmail.com>
2022-02-18 00:12:04 -0300

    Tab na alteração de empresa

--- src/core/helpers/validators.js
+++ src/core/helpers/validators.js
@@ -1,19 +1,17 @@
+/* eslint-disable */
 export const cpfValidator = (value) => {
     var Soma;
     var Resto;
     Soma = 0;
-    if (value == "00000000000") return false;
-
+    if (value == "00000000000000") return false;
     for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(value.substring(i - 1, i)) * (11 - i);
     Resto = (Soma * 10) % 11;
-
     if ((Resto == 10) || (Resto == 11)) Resto = 0;
     if (Resto != parseInt(value.substring(9, 10))) return false;
 
     Soma = 0;
     for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(value.substring(i - 1, i)) * (12 - i);
     Resto = (Soma * 10) % 11;
-
     if ((Resto == 10) || (Resto == 11)) Resto = 0;
     if (Resto != parseInt(value.substring(10, 11))) return false;
     return true;
@@ -21,6 +19,48 @@ export const cpfValidator = (value) => {
 
 export const cpfValidatorMask = (value) => {
     var numeroCPF = value.replace('.', '').replace('.', '').replace('-', '');
-    console.log(numeroCPF)
     return cpfValidator(numeroCPF);
+}
+
+export const cnpjValidatorMask = (value) => {
+    var numeroNCPJ = value.replace('.', '').replace('.', '').replace('.', '').replace('/', '').replace('-', '');
+    if(value == '') return false;
+    return cnpjValidator(numeroNCPJ);
+}
+
+function cnpjValidator(value) { 
+    if (value.length != 14)
+        return false;
+
+    if (value == "00000000000000")
+        return false;
+         
+    tamanho = value.length - 2
+    numeros = value.substring(0,tamanho);
+    var digitos = value.substring(tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(0))
+        return false;
+         
+   var tamanho = tamanho + 1;
+    var numeros = value.substring(0,tamanho);
+    var soma = 0;
+    var pos = tamanho - 7;
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2)
+            pos = 9;
+    }
+    var resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+    if (resultado != digitos.charAt(1))
+          return false;
+           
+    return true;
 }
--- src/views/pages/empresa/Alterar/index.jsx
+++ src/views/pages/empresa/Alterar/index.jsx
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect, useSelector } from 'react-redux';
 import * as actionsEmpresa from '../../../../domain/actions/actionsEmpresa';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import BotaoConfirmar from '../../../components/BotaoConfirmar';
 import Alerta from '../../../components/Alerta';
 import ModalCarregando from '../../../components/ModalCarregando';
-import { cnpjMask, cpfMask } from '../../../../core/helpers/masks';
-import { cpfValidatorMask } from '../../../../core/helpers/validators';
+import { cnpjMask, cpfMask, cepMask } from '../../../../core/helpers/masks';
+import { cpfValidatorMask, cnpjValidatorMask } from '../../../../core/helpers/validators';
 
 const EmpresaAlterar = (props) => {
-    const state = useSelector((state) => state.empresa);
-    const [tipoPessoa, setTipoPessoa] = useState(null);
+    const {empresa} = useSelector((state) => state.empresa);
     const [id, setId] = useState("");
     const [cpfCnpj, setCpfCnpj] = useState("");
     const [nome, setNome] = useState("");
     const [ativo, setAtivo] = useState(false);
+    const [logradouro, setLogradouro] = useState("");
+    const [numero, setNumero] = useState("");
+    const [complemento, setComplemento] = useState("");
+    const [bairro, setBairro] = useState("");
+    const [cep, setCep] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [estado, setEstado] = useState("");
     const [alerta, setAlerta] = useState("");
     const [mensagem, setMensagem] = useState("");
     const [aguardando, setAguardando] = useState(false);
+    const [tipoPessoa, setTipoPessoa] = useState(null);
+    const [tabAtiva, setTabAtiva] = useState(0);
 
     useEffect(() => {
         const { id } = props.match.params;
@@ -33,12 +41,19 @@ const EmpresaAlterar = (props) => {
 
     const receberDadosEmpresa = async (id) => {
         await props.getEmpresa({ id });
-        if (state.empresa) {
-            setId(state.empresa.id);
-            setNome(state.empresa.nome);
-            setCpfCnpj(state.empresa.cpfCnpj);
-            setAtivo(state.empresa.ativo);
-            setTipoPessoa(state.empresa.cpfCnpj && (Object.keys(state.empresa.cpfCnpj).length <= 11 ? "CPF" : "CNPJ"))
+        if (empresa) {
+            setId(empresa.id);
+            setNome(empresa.nome);
+            setCpfCnpj(empresa.cpfCnpj);
+            setAtivo(empresa.ativo);
+            setTipoPessoa(empresa.cpfCnpj && (Object.keys(empresa.cpfCnpj).length <= 11 ? "CPF" : "CNPJ"));
+            setLogradouro(empresa.endereco.logradouro);
+            setNumero(empresa.endereco.numero);
+            setBairro(empresa.endereco.bairro);
+            setCep(empresa.endereco.cep);
+            setComplemento(empresa.endereco.complemento);
+            setCidade(empresa.endereco.cidade.id);
+            setEstado(empresa.endereco.cidade.estado.id)
         }
     }
 
@@ -51,7 +66,21 @@ const EmpresaAlterar = (props) => {
         }
 
         setAguardando(true);
-
+        let dadosEmpresa = {
+            id, 
+            nome,
+            cpfCnpj, 
+            telefone: null, 
+            endereco: {
+                logradouro, 
+                numero, 
+                complemento, 
+                bairro, 
+                cidade: { 
+                    id: cidade 
+                }
+            }
+        }
         // props.alterarEmpresa({ id, nome, cpfCnpj, ativo }, (retorno) => {
         //     if (retorno.erro) {
         //         setAlerta("warning");
@@ -70,15 +99,38 @@ const EmpresaAlterar = (props) => {
     }
 
     const criticas = () => {
-        if (!nome) return setMensagem("Preencha o campo nome!");
-        if (!cpfCnpj) return setMensagem("Preencha o campo CPF/CNPJ!");
-        if (!cpfValidatorMask(cpfCnpj)) return setMensagem("CPF/CNPJ inválido!");
-        return true;
+        /* eslint-disable */
+        if (!nome) {
+            return setMensagem("Preencha o campo nome!");
+        } else if (!cpfCnpj) {
+            return setMensagem("Preencha o campo CPF/CNPJ!");
+        } else if (tipoPessoa == "CPF" && !cpfValidatorMask(cpfCnpj)) {
+            return setMensagem("CPF inválido!")
+        } else if (tipoPessoa == "CNPJ" && !cnpjValidatorMask(cpfCnpj)) {
+            return setMensagem("CNPJ inválido!")
+        } else {
+            return true;
+        }
+        /* eslint-enable */
+    }
+
+    const ativaTabBar = (id) => {
+        let componentes = document.getElementsByClassName('dados-cadastrais');
+        let indiceComponente = null;
+        for (let i = 0; i < componentes.length; i++) {
+            componentes[i].classList.remove('active');
+            if (componentes[i].id == id) {
+                indiceComponente = i;
+            }
+        }
+        document.getElementById(id).classList.add('active');
+        setTabAtiva(indiceComponente);
     }
 
     return (
         <>
-            {!state.empresa && <ModalCarregando aguardando={true} pagina="Buscando informações da empresa." />}
+        {/* {console.log(empresa)} */}
+            {!empresa && <ModalCarregando aguardando={true} pagina="Buscando informações da empresa." />}
             <div className="d-flex justify-content-between">
                 <div className="mr-auto p-2">
                     <h2 className="display-4 titulo">Alterar Empresa</h2>
@@ -98,72 +150,187 @@ const EmpresaAlterar = (props) => {
             </div>
             <hr />
             {mensagem ? <Alerta tipoAlerta={alerta} mensagem={mensagem} /> : null}
-            <Form>
-                <Input type="hidden"
-                    value={id}
-                    name="id"
-                    id="id" />
-                <FormGroup>
-                    <Label for="nome">Nome</Label>
-                    <Input
-                        type="text"
-                        value={nome}
-                        name="nome"
-                        id="nome"
-                        className="form-control"
-                        //placeholder={dadosEmpresa ? "Nome do usuário" : "Carregado..."}
-                        //disabled={dadosEmpresa ? false : true}
-                        autoComplete="nome"
-                        onChange={ev => setNome(ev.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <span className='p-2'>
-                        <Input name="cpf_cnpj" type="radio" onChange={() => setTipoPessoa("CPF")} checked={tipoPessoa == "CPF"} />
-                        <Label className='ms-1' check>CPF</Label>
-                    </span>
-                    <span className='p-2'>
-                        <Input name="cpf_cnpj" type="radio" onChange={() => setTipoPessoa("CNPJ")} checked={tipoPessoa == "CNPJ"} />
-                        <Label className='ms-1' check>CNPJ</Label>
-                    </span>
-                    <Input
-                        type="text"
-                        value={cpfCnpj}
-                        name="cpfCnpj"
-                        id="cpfCnpj"
-                        className="form-control"
-                        //placeholder={dadosEmpresa ? "E-mail do usuário" : "Carregando.."}
-                        // disabled={dadosEmpresa ? false : true}
-                        autoComplete="cpfCnpj"
-                        onChange={(ev) => {
-                            if (tipoPessoa == "CPF") {
-                                setCpfCnpj(cpfMask(ev.target.value));
-                            } else {
-                                setCpfCnpj(cnpjMask(ev.target.value));
-                            }
-                        }}
-                        disabled={!tipoPessoa}
-                    />
-                </FormGroup>
-                <FormGroup check inline>
-                    <Label for="ativo" check>
-                        <Input
-                            type="checkbox"
-                            checked={ativo ? true : false}
-                            value={ativo}
-                            name="ativo"
-                            id="ativo"
-                            //disabled={dadosEmpresa ? false : true}
-                            autoComplete="ativo"
-                            onClick={() => (ativo ? setAtivo(false) : setAtivo(true))}
-                        /> Ativo
-                    </Label>
-                </FormGroup>
-                <br /><br />
-                <Link onClick={alterarEmpresa} to="#">
-                    <BotaoConfirmar aguardando={aguardando} />
-                </Link>
-            </Form>
+            <div>
+                <Form>
+                    <Input type="hidden" value={id} name="id" id="id" />
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink id="empresa" className="active cursor dados-cadastrais" onClick={ev => ativaTabBar(ev.target.id)}>
+                                Dados Empresa
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink id="endereco" className="cursor dados-cadastrais" onClick={ev => ativaTabBar(ev.target.id)}>
+                                Endereço
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={tabAtiva}>
+                        <TabPane tabId={0}>
+                            <FormGroup>
+                                <Label for="nome">Nome</Label>
+                                <Input
+                                    type="text"
+                                    value={nome}
+                                    name="nome"
+                                    id="nome"
+                                    className="form-control"
+                                    autoComplete="nome"
+                                    onChange={ev => setNome(ev.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <span className='p-2'>
+                                    <Input name="cpf_cnpj" type="radio" onChange={() => setTipoPessoa("CPF")} checked={tipoPessoa == "CPF"} />
+                                    <Label className='ms-1' for='cpf_cnpj' check>CPF</Label>
+                                </span>
+                                <span className='p-2'>
+                                    <Input name="cpf_cnpj" type="radio" onChange={() => setTipoPessoa("CNPJ")} checked={tipoPessoa == "CNPJ"} />
+                                    <Label className='ms-1' for='cpf_cnpj' check>CNPJ</Label>
+                                </span>
+                                <Input
+                                    type="text"
+                                    value={cpfCnpj}
+                                    name="cpfCnpj"
+                                    id="cpfCnpj"
+                                    className="form-control"
+                                    autoComplete="cpfCnpj"
+                                    onChange={ev => {
+                                        if (tipoPessoa == "CPF") {
+                                            setCpfCnpj(cpfMask(ev.target.value));
+                                        } else {
+                                            setCpfCnpj(cnpjMask(ev.target.value));
+                                        }
+                                    }}
+                                    disabled={!tipoPessoa}
+                                />
+                            </FormGroup>
+                            <FormGroup check inline>
+                                <Label for="ativo" check>
+                                    <Input
+                                        type="checkbox"
+                                        checked={ativo ? true : false}
+                                        value={ativo}
+                                        name="ativo"
+                                        id="ativo"
+                                        autoComplete="ativo"
+                                        onClick={() => (ativo ? setAtivo(false) : setAtivo(true))}
+                                    /> Ativo
+                                </Label>
+                            </FormGroup>
+                        </TabPane>
+                        <TabPane tabId={1}>
+                            <FormGroup>
+                                <Label for="logradouro">Rua/Av</Label>
+                                <Input
+                                    type="text"
+                                    value={logradouro}
+                                    name="logradouro"
+                                    id="logradouro"
+                                    className="form-control"
+                                    autoComplete="logradouro"
+                                    onChange={ev => setLogradouro(ev.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="numero">Número</Label>
+                                <Input
+                                    type="text"
+                                    value={numero}
+                                    name="numero"
+                                    id="numero"
+                                    className="form-control"
+                                    autoComplete="numero"
+                                    onChange={ev => setNumero(ev.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="complemento">Complemento</Label>
+                                <Input
+                                    type="text"
+                                    value={complemento}
+                                    name="complemento"
+                                    id="complemento"
+                                    className="form-control"
+                                    autoComplete="complemento"
+                                    onChange={ev => setComplemento(ev.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="bairro">Bairro</Label>
+                                <Input
+                                    type="text"
+                                    value={bairro}
+                                    name="bairro"
+                                    id="bairro"
+                                    className="form-control"
+                                    autoComplete="bairro"
+                                    onChange={ev => setBairro(ev.target.value)}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="cep">CEP</Label>
+                                <Input
+                                    type="text"
+                                    value={cep}
+                                    name="cep"
+                                    id="cep"
+                                    className="form-control"
+                                    autoComplete="cep"
+                                    onChange={ev => setCep(cepMask(ev.target.value))}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="bairro">Cidade</Label>
+                                <Input
+                                    bsSize="lg"
+                                    className="mb-3"
+                                    type="select"
+                                    value={cidade}
+                                    onChange={ev=>setCidade(ev.target.value)}
+                                >
+                                    <option value={0}>
+                                       
+                                    </option>   
+                                    
+                                       <option value={1}>
+                                       cidade1
+                                    </option>
+                                    <option value={2}>
+                                      cidade2
+                                    </option>
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="bairro">Estado</Label>
+                                <Input
+                                    bsSize="lg"
+                                    className="mb-3"
+                                    type="select"
+                                    value={estado}
+                                    onChange={ev=>setEstado(ev.target.value)}
+                                >
+                                    <option value={0}>
+                                       
+                                    </option>   
+                                    
+                                       <option value={1}>
+                                       estado1
+                                    </option>
+                                    <option value={1}>
+                                      estado2
+                                    </option>
+                                </Input>
+                            </FormGroup>
+                        </TabPane>
+                    </TabContent>
+                    <div>
+                        <Link onClick={alterarEmpresa} to="#">
+                            <BotaoConfirmar aguardando={aguardando} />
+                        </Link>
+                    </div>
+                </Form>
+            </div>
         </>
     )
 }
